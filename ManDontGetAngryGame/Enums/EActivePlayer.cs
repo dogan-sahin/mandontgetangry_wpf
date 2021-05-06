954c581f9c944b50740b51cf987299432d8112f6
dennis12 <dennis.krobath@students.fhv.at>
2021-05-06 09:35:28 +0200

    enum eactiveplayer

---
+++ ManDontGetAngryGame/Enums/EActivePlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManDontGetAngryGame.Enums
+{
+    enum EActivePlayer
+    {
+        Green,
+        Blue,
+        Red,
+        Yellow
+    }
+}
