b4f2cdf5d70a3da40c4c24e1b7fa33c79a63288d
Göni <simonf.goenitzer@gmail.com>
2021-05-06 10:46:16 +0200

    Adde view model interface and command relay functionality

---
+++ ManDontGetAngryGame/ViewModels/ICellStatusViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using ManDontGetAngryGame.Enums;
+using ManDontGetAngryGame.Model;
+
+namespace ManDontGetAngryGame.ViewModels
+{
+    interface ICellStatusViewModel : INotifyPropertyChanged
+    {
+        EPieceColor PieceColor { get; }
+
+        bool isEmpty { get; }
+
+        ECellType CellType { get; }
+
+        CellId Identifier { get; }
+
+        void SetPiece(EPieceColor pieceColor);
+
+        bool isCellCurrentlySelected { get; set; }
+
+        bool isValidTarget { get; set; }
+
+        event EventHandler<CellId> SelectedCell;
+
+        RelayCommand CellSelectedCommand { get; }
+
+    }
+}
