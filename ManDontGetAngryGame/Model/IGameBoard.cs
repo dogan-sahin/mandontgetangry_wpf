c3d89abc4890bbaa4a6c2aa4e5945bf1fcbc8228
Dogan Sahin <dsa5439@students.fhv.at>
2021-05-06 09:47:27 +0200

    gamelogic

---
+++ ManDontGetAngryGame/Model/IGameBoard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManDontGetAngryGame.Model
+{
+    interface IGameBoard
+    {
+        int NumRows { get; }
+
+        int NumCols { get; }
+
+       // event EventHandler<CellStatusChangedEventArgs> CellStatusChanged;
+
+        void InitializeBoard();
+
+        CellModel GetCell(CellId cellId);
+
+        bool IsValidMove(CellId source, CellId target);
+
+        void MovePiece(CellId source, CellId target);
+
+        void SwapPiece(CellId source, CellId target);
+
+        bool HasMoved(CellId cell);
+    }
+}
